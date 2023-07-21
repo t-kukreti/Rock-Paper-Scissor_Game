a9f9d478c3ef963c3586afba3736058b10e5173d
t-kukreti <tarunkukreti789@gmail.com>
2023-07-22 02:13:56 +0530

    Update script.js
    modified script, still need to work on its error-handling

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 const rockPaperScissorGame = (() => {
-    
     let userScore = 0;
     let machineScore = 0;
+    let userName = '';
+    const textBox = document.querySelector('#textBox');
+    const finalResult = document.querySelector('#finalResult');
+    const playerName = document.querySelector('#playerName');
     const playerBtn = document.querySelectorAll('.player-Btn');
+    const computerBtn = document.querySelectorAll('.pc-player-Btn');
     const playerScore = document.querySelector('#player-score');
     const computerScore = document.querySelector('#computer-score');
-    const startGameBtn = document.querySelector('#startGame-Btn');
+    const startGameBtn = document.querySelector('.Game-Btn'); // same class name as restart button check later for bugs
     const restartGameBtn = document.createElement('button');
+
+    const displayMessageIfPcButtonClicked = ()=>{
+        alert(`apne dabe mai khel le bro (don't click pc butons)`);
+    };
 
     const getComputerChoice = () => {
         const gameVals = ['Rock', 'Paper', 'Scissors'];
         const randomIndex = Math.floor(Math.random() * gameVals.length);
-        return gameVals[randomIndex];
+        return{
+            gameVals,  
+            randomIndex
+        }
+    };
+
+    const getPlayerName = ()=>{
+        // make this only return a valid name, it should just contain a character.
+        let name='';
+        while(!(name.length > 0)){
+            name = prompt('whats your name');
+        }
+        return name;
     };
 
     const getUserChoice = (element) => {
@@ -27,13 +47,37 @@ const rockPaperScissorGame = (() => {
         startGameBtn.disabled = false;
         userScore = 0;
         machineScore = 0;
-        playerScore.textContent = `playerScore: ${userScore}`;
-        computerScore.textContent = `machineScore: ${machineScore}`;
-        document.body.removeChild(restartGameBtn);
+        textBox.textContent = '';
+        finalResult.textContent = '';
+        playerScore.textContent = `${userName}: ${userScore}`;
+        computerScore.textContent = `Pc: ${machineScore}`;
+        // removing color classes which were used to add colors to button, on clicking.
+        resetColorClasses(computerBtn);
+        resetColorClasses(playerBtn);
+        //remove restart button
+        document.querySelector('footer').removeChild(restartGameBtn);
+        // show start button
+        startGameBtn.classList.remove('hide');
     };
 
     const togglePlayerButtons = (playerBtn, value) => {
         playerBtn.forEach((btn) => { btn.disabled = value; });
+    };
+
+    const addNonexistentClass = (NodeList, nameOfClass, placeToadd) => {
+        NodeList.forEach((node)=>{
+            if(node.classList.contains(nameOfClass)){
+                node.classList.remove(nameOfClass);
+            }
+        });
+        placeToadd.classList.add(nameOfClass);
+    };
+
+    const resetColorClasses = (NodeList)=>{
+        // not added strict conditions as there were no extra class apart from color ones.
+        NodeList.forEach((element)=>{
+            element.removeAttribute('class');
+        });
     };
 
     const playRound = (e) => {
@@ -55,38 +99,62 @@ const rockPaperScissorGame = (() => {
                 scissors: 'Computer choosed scissors, it is a Tie!'
             }
         };
-
-        const computerChoice = getComputerChoice().toLowerCase();
+        const compIndex = getComputerChoice().randomIndex;
+        const computerChoice = getComputerChoice().gameVals[compIndex].toLowerCase();
         const userChoice = getUserChoice(e.target);
 
+        addNonexistentClass(playerBtn,'yellowColor',e.target);
+        addNonexistentClass(computerBtn,'redColor',computerBtn[compIndex]);
+
+
         if (outcomes[userChoice][computerChoice].includes('won')) {
-            playerScore.textContent = `playerScore: ${++userScore}`;
+            textBox.textContent = outcomes[userChoice][computerChoice];
+            playerScore.textContent = `${userName}: ${++userScore}`;
         }
         else if (outcomes[userChoice][computerChoice].includes('lose')) {
-            computerScore.textContent = `computerScore: ${++machineScore}`
+            textBox.textContent = outcomes[userChoice][computerChoice];
+            computerScore.textContent = `Pc: ${++machineScore}`
+        }
+        else if(outcomes[userChoice][computerChoice].includes('Tie')){
+            textBox.textContent = outcomes[userChoice][computerChoice];
         }
 
-        // elimination condition, could be dynamic change if u want to
+        // elimination condition, could be dynamic change if u want to, make a function for this.
         if (userScore + machineScore >= 5) {
             togglePlayerButtons(playerBtn, true);
             restartGameBtn.textContent = 'Restart Game';
-            restartGameBtn.setAttribute('style','font-size: 2rem;');
-            document.body.appendChild(restartGameBtn);
+            restartGameBtn.className = 'Game-Btn';
+            restartGameBtn.setAttribute('style','color: red; background-color: #4e4343;');
+            document.querySelector('footer').appendChild(restartGameBtn);
+            if(userScore > machineScore){
+                finalResult.style.color = 'green';
+                finalResult.textContent = `Ultimately ${userName} won the series of 5 game! congratulations`;
+            }
+            else{
+                finalResult.style.color = 'red';
+                finalResult.textContent = `Alas ${userName} lose the series of 5 game, but you can surely beat it next time!`;
+            }
         }
     };
 
     const game = () => {
+
         startGameBtn.disabled = true;
+        startGameBtn.classList.add('hide'); 
         // opening player button to capture click
         togglePlayerButtons(playerBtn, false);
-
         playerBtn.forEach((button) => {
             button.addEventListener('click', playRound);
         });
-
+        computerBtn.forEach((btn)=>{btn.addEventListener('click',displayMessageIfPcButtonClicked)});
     };
 
     const init = () => {
+        document.addEventListener('DOMContentLoaded',()=>{
+            userName = getPlayerName();
+            playerName.textContent = userName;
+            playerScore.textContent = userName;
+        });
         startGameBtn.addEventListener('click', game);
         restartGameBtn.addEventListener('click', restartGame);
     };
